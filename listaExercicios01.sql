73f8338925a91529caa3db1dc676863004d60dec
EA - Jean Mendes Da Silva <jean_mendes@educadventista.org>
2023-09-05 17:56:04 -0300

    exercicio 5

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -16,3 +16,8 @@ select alunos.nome, matriculas.curso
 from alunos
 inner join matriculas on alunos.id = matriculas.aluno_id
 where matriculas.curso = 'Engenharia de Software';
+
+-- 5
+select produto, sum(receita) as receita_total
+from vendas
+group by produto;
